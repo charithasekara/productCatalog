40b5af83c274bed10989b9532d61a3e60bf79b80
Charitha Weerasekara <charithmadhushansekara@gmail.com>
2025-11-06 09:51:55 +0530

    update program.cs
    update program.cs

--- ProductCatalog.API/Program.cs
+++ ProductCatalog.API/Program.cs
@@ -1,24 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ProductCatalog.Application.Products.Queries.GetAllProductsQuery).Assembly));
 
-// Get connection string
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-// Register Infrastructure services
+
 builder.Services.AddSingleton(new ProductCatalog.Infrastructure.Data.DatabaseConnectionFactory(connectionString));
 builder.Services.AddScoped<ProductCatalog.Application.Interfaces.IProductRepository, ProductCatalog.Infrastructure.Data.ProductRepository>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -27,7 +24,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
